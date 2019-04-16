9c72d6f2a346b28954e8d29846f7a3bb30a980f8
Senvr <Leeeyecard@gmail.com>
2019-04-15 19:37:13 -0700

    ARDUINO 1-3: Fixed, Changes
    Reverted last change cause it was poopyhead
    Returns serial data as an x,y format instead
    Accurate lag accounting (in microseconds), an actual variable for tightening and shit. CoooOOoool

--- arduino-code/main.ino
+++ arduino-code/main.ino
@@ -7,21 +7,21 @@ const int screamPin = 5; // "I see something" Pin
 const int servoPin=9; // Servo Control Pin
 
 //VARIABLE SETUP HERE
-const int maxdist = 15; // Hard limit on the distance. Won't ACT (getdist() WILL return still) if the distance is over this limit.
+const int tightening=100000; // Change this if you want to "tighten" the delays 
 const int resolution=4; //Resolution. Use whole, positive numbers. The higher it goes, the faster it will go, but the less data will be given. Ideal values will be divisible by 180 or whateverr
 
 //EVERYTHING ELSE
 unsigned long starttime1 = 0; 
 unsigned long lag = 0;
-int ang;
+float ang;
 long duration = 0;
 float distance = 0;
 bool scream = true;
 int dist;
-unsigned long demoDuration=0; 
 Servo servo1; 
 
 void setup() {
+  Serial.begin(9600); // start serial communication
   Serial.println("#00000"); // Signal to anything listening that we just started/restart
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
@@ -29,11 +29,12 @@ void setup() {
   pinMode(screamPin, OUTPUT); // Sets the Scream Pin as an Output
   servo1.attach(servoPin); // Tell the servo library to attach to the aformentioned servo defined previously
   servo1.write(1); // Set it to a default angle of 1
+  
 }
 
 //Defined function to read sensor data. I did this so I could make things look and run a little better, and account for "lag" better.
 float getdist() { // Float 'cause of fancy schmancy decimals 'n shit
-  starttime1 = millis(); //Start off by notating how many miliseconds from "relative epoch" (epoch being begin of arduino's execution iirc) we are before all of this.
+  
   
   digitalWrite(trigPin, LOW);  // 
   delayMicroseconds(5);        // All of this shit here sends out a pulse to the ultrasonic sensor. 
@@ -41,16 +42,13 @@ float getdist() { // Float 'cause of fancy schmancy decimals 'n shit
   delayMicroseconds(10);       // 
   digitalWrite(trigPin, LOW);  //
 
-  duration = pulseIn(echoPin, HIGH, 10000); //This is the juicy bit, where it uses the arudino "pulseIn" to have the sensor return data from the sensor. Can't tell you 100% how the sensor does it, but we get a duration. 
-                                   //^^^ This timeout, 10 miliseconds, also determines the max distance. Keep this in mind if you decide to tamper with this in "tightening"
-  distance = duration * 0.034 / 2; // Do some physics math to find the distance based on the speed of sound (we want distance after all)
+  starttime1 = micros(); //See how long it took for pulsein to get data
   
-  if ((micros() - starttime1) < 10000) { // Make sure lag doesn't end up as a value higher than the delay. Also - IF YOU ARE TIGHTENING THE DELAYS, MAKE SURE YOU CHANGE THIS!!! Not killer, but advisable still.
-    lag = (micros() - starttime1); // See how many miliseconds this entire thing took. Will n
-  } else {
-    lag = 0; // Otherwise set to zero so it doesn't do any real change
-  }
-  starttime1 = 0; // Reset this, not sure why its required but ill figure this out later
+  duration = pulseIn(echoPin, HIGH, tightening); //This is the juicy bit, where it uses the arudino "pulseIn" to have the sensor return data from the sensor. Can't tell you 100% how the sensor does it, but we get a duration. 
+  delayMicroseconds(tightening-(micros()-starttime1));
+
+  
+  distance = duration * 0.034 / 2; // Do some physics math to find the distance based on the speed of sound (we want distance after all)
   return distance; // End this function by returning the distance.
 }
 
@@ -61,32 +59,23 @@ void incServo() {
   } else {
     ang=0; // Reset to the starting position. You can set a range here if you'd like. Use negative numbers to scare your servo.
     servo1.write(ang); // Set it to 0 right now, as there will be a 1 second delay before the reset if we don't. Just figure it'd be nice. Not required.
-    Serial.println("\n\n\n\n\n\n\n\n\n#00000"); // Notate we completed a full cycle. Same "signal" as the thing in start()
+    Serial.println("#00000"); // Notate we completed a full cycle. Same "signal" as the thing in start()
     delay(1000); // Wait a second. Lots of time to see that this ended and act accordingly. 
   }
   servo1.write(ang); // By now, we would have had a value set. This, as seen before, writes it to the servo.
-  delay(50); // delay, as to not tell the servo to step faster than it can physically move
+  delay(10*resolution); // delay, as to not tell the servo to step faster than it can physically move
+  
 }
 
 
 //Now: To execute all of this.
 void loop() {
-  Serial.println("=============================="); // DEBUGGING
-  demoDuration=micros(); // same lag thing as before but this is actually important (i guess)
   distance = getdist(); // Runs the sensor, puts the output as the distance. Probably redundant. Don't care.
-  Serial.print("Distance: "); // NOT DEBUGGING!!! This sends the "header" over first. Required to differentiate stuff 
-  Serial.println(distance); // The meaty goodness: the entire fucking point of this script all lead up to basically this line.
-
-  delayMicroseconds(10000 - lag); // Account for lag, lag being from the getdist thing. 
-  Serial.print("Lag:"); // Same header thing
-  Serial.println(lag); // beep boop same fuckn shit
-  
+  Serial.print(ang);       // X         |  This is done to give a "coordinate" based on the step and the distance from the sensor.
+  Serial.print(",");       // ,         |  Organized for easy programming. Subject to change. 
+  Serial.println(distance);// Y         |  Eat my poopsack
+    
   incServo(); // Move the servo forward after data has been read.
-  if((micros()-demoDuration) < 100000){ // Same lag-check as before and all that.
-  delayMicroseconds(100000-(micros()-demoDuration)); // This equalizes each loop time (Relatively speaking. It's off by 1-2 miliseconds as the CPU isnt exactly a 4 gigahertz intel I7 (my hypothesis atleast). A lot better than the 2-9 miliseconds. Increaces ACCURACY
-  }
-  Serial.print("Cycle Time:"); // Headerass
-  Serial.println(micros()-demoDuration); // aaa
-  demoDuration=0; // reset duration
-  Serial.println("=============================="); //Debug Dickshit
+
+  
 }
